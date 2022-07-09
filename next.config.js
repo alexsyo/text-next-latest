00df4b33e48f7e5b364d08cd7dabcad17a550314
alexs <alex_ok@hotmail.it>
2022-07-09 03:40:36 +0200

    test updated next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false;
+    // config.optimization.minimizer = []; // todo: remove once opensea works correctly -> https://github.com/ProjectOpenSea/opensea-js/issues/451
+    return config;
+  }
 }
 
 module.exports = nextConfig
